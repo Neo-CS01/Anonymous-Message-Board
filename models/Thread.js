872a8a359f538a46f2514d72b51a6592c518c7db
Glitch (enchanted-concrete-girdle) <none>
2024-07-06 12:56:08 +0000

    🐟🚍 Checkpoint ./server.js:68384043/4650 ./models/Thread.js:68384043/1395

--- models/Thread.js
+++ models/Thread.js
@@ -1,30 +1,22 @@
-const mongoose = require("mongoose");
-
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
 
-const ThreadSchema = new Schema(
-  {
-    board: { type: String, required: true },
-    replies: [{ type: ObjectId, ref: "Reply" }],
-    text: { type: String, required: true },
-    delete_password: { type: String, required: true },
-    reported: { type: Boolean, default: false },
-    bumped_on: { type: Date, default: Date.now },
-    created_on: { type: Date, default: Date.now },
-  },
-  {
-    // timestamps: { createdAt: "created_on", updatedAt: "bumped_on" },
-    versionKey: false
-  }
-);
+const replySchema = new Schema({
+  text: String,
+  delete_password: String,
+  created_on: { type: Date, default: Date.now },
+  reported: { type: Boolean, default: false }
+});
 
-ThreadSchema.methods.toJSON = function () {
-  var obj = this.toObject();
-  delete obj.delete_password;
-  delete obj.reported;
-  delete obj.board;
-  return obj
-}
+const threadSchema = new Schema({
+  text: String,
+  delete_password: String,
+  created_on: { type: Date, default: Date.now },
+  bumped_on: { type: Date, default: Date.now },
+  reported: { type: Boolean, default: false },
+  replies: [replySchema]
+});
 
-module.exports = mongoose.model("Thread", ThreadSchema);
+const Thread = mongoose.model('Thread', threadSchema);
+
+module.exports = Thread;
