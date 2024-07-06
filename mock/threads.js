8cddee07ff0460d401ef73c0d1c8f429d6d4e42f
Glitch (enchanted-concrete-girdle) <none>
2024-07-06 13:40:40 +0000

    🏔✌️ Checkpoint ./mock/threads.js:68384043/648

--- mock/threads.js
+++ mock/threads.js
@@ -1,4 +1,23 @@
-const threads = [];
+const threads = [
+  {
+    _id: 'thread_id_1',
+    text: 'First thread',
+    created_on: new Date(),
+    bumped_on: new Date(),
+    reported: false,
+    delete_password: 'password',
+    replies: []
+  },
+  {
+    _id: 'thread_id_2',
+    text: 'Second thread',
+    created_on: new Date(),
+    bumped_on: new Date(),
+    reported: false,
+    delete_password: 'password',
+    replies: []
+  }
+];
 
 exports.threads = threads;
 
